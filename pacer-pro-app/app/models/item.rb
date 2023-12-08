2a0db35e7e403463143defd02fa604cac027d3d1
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-12-08 03:16:43 -0500

    implement soft delete

--- pacer-pro-app/app/models/item.rb
+++ pacer-pro-app/app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+    # Validations
+    validates :name, presence: true
+  
+    # Marks the item as deleted by setting the deleted_at timestamp
+    def soft_delete
+      update(deleted_at: Time.current)
+    end
+  
+    # Restores the item by setting the deleted_at to nil
+    def restore
+      update(deleted_at: nil)
+    end
 end
+  
